7c188362fef6c4044a9308e9ff8d42cb21720f4a
h_kael <dbxh613@gmail.com>
2026-09-18 14:31:55 +0900

    오늘의 세계관 업데이트 - 2026-09-18 (금) 14:31:54.63

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,9 +45,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.Graph(),
   ],
 }
 
